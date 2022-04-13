e3c63afaac5cebfcb0029607576c176f320992a6
nathanwooo <nathanwooo1231@gmail.com>
2022-04-13 21:08:47 +0800

    add create hku members

--- coreapi/views.py
+++ coreapi/views.py
@@ -11,11 +11,31 @@ def ListHkuMember(request):
         display += str(hkumember)+"<br>"
     
     return HttpResponse(display)
-def CreateHkuMember(request):
-    return HttpResponse("display")
+def CreateHkuMember(hku_id,name):
+    p = HkuMember.objects.create(hku_id=hku_id, name=name)
+    
+    display=""
+    allhkumember = HkuMember.objects.all()
+    for hkumember in allhkumember:
+        display += str(hkumember)+"<br>"
+    return HttpResponse(display)
+
+#not done yet for the following functions
 def ViewHkuMember(request):
-    return HttpResponse("display")
+    display=""
+    allhkumember = HkuMember.objects.all()
+    for hkumember in allhkumember:
+        display += str(hkumember)+"<br>"
+    return HttpResponse(display)
 def ModifyHkuMember(request):
-    return HttpResponse("display")
+    display=""
+    allhkumember = HkuMember.objects.all()
+    for hkumember in allhkumember:
+        display += str(hkumember)+"<br>"
+    return HttpResponse(display)
 def DeleteHkuMember(request):
-    return HttpResponse("display")
+    display=""
+    allhkumember = HkuMember.objects.all()
+    for hkumember in allhkumember:
+        display += str(hkumember)+"<br>"
+    return HttpResponse(display)
