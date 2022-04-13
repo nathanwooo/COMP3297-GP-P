c2f736b0f1a60f2014070e6da3d407b3107cba22
nathanwooo <nathanwooo1231@gmail.com>
2022-04-14 02:10:58 +0800

    return json now

--- core/settings.py
+++ core/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framework',
     'coreapi.apps.CoreapiConfig',
     'django.contrib.admin',
     'django.contrib.auth',
--- coreapi/views.py
+++ coreapi/views.py
@@ -1,16 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from coreapi.models import Venue, HkuMember, Visit
 # Create your views here.
-
+@api_view(['GET',])
 def HkuMembers(request):
     display=""
     allhkumember = HkuMember.objects.all()
     for hkumember in allhkumember:
         display += str(hkumember)+"<br>"
     
-    return HttpResponse(display)
+    return Response({"result":display})
 def CreateHkuMember(hku_id,name):
     p = HkuMember.objects.create(hku_id=hku_id, name=name)
     
